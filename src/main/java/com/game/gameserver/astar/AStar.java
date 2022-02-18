dcae5d47eb58bf6af6cb05dd918533b5e2e1d483
yvgenchemeris <cheneris.lunat1k@gmail.com>
2022-02-18 18:51:10 +0200

    commented code removed

--- src/main/java/com/game/gameserver/astar/AStar.java
+++ src/main/java/com/game/gameserver/astar/AStar.java
@@ -95,12 +95,6 @@ public class AStar {
         int col = currentNode.getCol();
         int lowerRow = row + 1;
         if (lowerRow < getSearchArea().length) {
-            if (col - 1 >= 0) {
-//                checkNode(currentNode, col - 1, lowerRow, getDiagonalCost()); // Comment this line if diagonal movements are not allowed
-            }
-            if (col + 1 < getSearchArea()[0].length) {
-//                checkNode(currentNode, col + 1, lowerRow, getDiagonalCost()); // Comment this line if diagonal movements are not allowed
-            }
             checkNode(currentNode, col, lowerRow, getHvCost());
         }
     }
@@ -122,12 +116,6 @@ public class AStar {
         int col = currentNode.getCol();
         int upperRow = row - 1;
         if (upperRow >= 0) {
-            if (col - 1 >= 0) {
-//                checkNode(currentNode, col - 1, upperRow, getDiagonalCost()); // Comment this if diagonal movements are not allowed
-            }
-            if (col + 1 < getSearchArea()[0].length) {
-//                checkNode(currentNode, col + 1, upperRow, getDiagonalCost()); // Comment this if diagonal movements are not allowed
-            }
             checkNode(currentNode, col, upperRow, getHvCost());
         }
     }
